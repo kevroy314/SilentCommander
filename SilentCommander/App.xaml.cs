4e21683badc4889cfc124cc69d20a9af8ef17449
Kevin Horecka <kevin.horecka@gmail.com>
2021-12-26 16:08:05 -0600

    working version

--- SilentCommander/App.xaml.cs
+++ SilentCommander/App.xaml.cs
@@ -1,28 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
 
 namespace SilentCommander
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
         public static string[] Args;
         public static Dictionary<string, object> parsedArgs;
         void app_Startup(object sender, StartupEventArgs e)
         {
-            // If no command line arguments were provided, don't process them if (e.Args.Length == 0) return;  
+            parsedArgs = new Dictionary<string, object>();
+            // If no command line arguments were provided, don't process them if (e.Args.Length == 0) return;
+            parsedArgs = new Dictionary<string, object>();
+            parsedArgs["waittime"] = -1f;
+            parsedArgs["wait"] = false;
             if (e.Args.Length > 0)
             {
                 Args = e.Args;
-                parsedArgs = new Dictionary<string, object>();
+                for (int i = 0; i < Args.Length - 1; i++)
+                {
+                    switch (Args[i])
+                    {
+                        case "-w":
+                            parsedArgs["wait"] = true;
+                            break;
+                        case "-t":
+                            parsedArgs["waittime"] = int.Parse(Args[i + 1]);
+                            i++; // skip next arg as we just read it
+                            break;
+                    }
+                }
                 parsedArgs.Add("cmd", Args[Args.Length - 1]);
+                Process myProcess = new Process();
+                myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                myProcess.StartInfo.CreateNoWindow = true;
+                myProcess.StartInfo.UseShellExecute = false;
+                myProcess.StartInfo.FileName = "cmd.exe";
+                myProcess.StartInfo.Arguments = "/c " + "\"" + (string)App.parsedArgs["cmd"] + "\"";
+                myProcess.EnableRaisingEvents = true;
+                myProcess.Start();
+                if ((bool)App.parsedArgs["wait"])
+                {
+                    if ((int)App.parsedArgs["waittime"] < 0)
+                        myProcess.WaitForExit();
+                    else
+                        myProcess.WaitForExit((int)App.parsedArgs["waittime"]);
+                }
             }
         }
     }
--- SilentCommander/MainWindow.xaml.cs
+++ SilentCommander/MainWindow.xaml.cs
@@ -1,63 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace SilentCommander
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
-        const uint WS_EX_TRANSPARENT = 0x00000020;
-        const int GWL_EXSTYLE = (-20);
-        const uint WS_EX_TOOLWINDOW = 0x00000080;
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
-        static extern uint GetWindowLongPtr(int hWnd, int nIndex);
-
-        [DllImport("user32.dll")]
-        static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
-
-        public static void SetWindowExTransparent(IntPtr hwnd)
-        {
-            uint extendedStyle = GetWindowLongPtr(hwnd.ToInt32(), -20);
-            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW);
-        }
-        protected override void OnSourceInitialized(EventArgs e)
-        {
-            base.OnSourceInitialized(e);
-            var hwnd = new WindowInteropHelper(this).Handle;
-            SetWindowExTransparent(hwnd);
-        }
+        public MainWindow() { }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            int pad = 16;
-            this.Width = System.Windows.SystemParameters.PrimaryScreenWidth + pad;
-            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight + pad;
-            this.Left = -pad / 2;
-            this.Top = -pad / 2;
-            this.Padding = new Thickness(0);
-            this.Margin = new Thickness(0);
-            this.Topmost = true;
+            Close();
         }
     }
 }
